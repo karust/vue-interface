eecb1da5d527f893c9bfb3b89782e3478607392e
Rust <karust13@gmail.com>
2019-02-15 21:22:55 +0300

    dist

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import 'vuetify/dist/vuetify.min.css'
 
 
 Vue.config.productionTip = false
-Vue.prototype.$hostname = 'http://10.1.1.192:5000/'
+Vue.prototype.$hostname = 'http://127.0.0.1:5000/'
 
 Vue.use(Vuetify)
 Vue.use(VueResource);
