746410d02248dfc4ea7f56ab7fbafd8b09129d23
Rust <karust13@gmail.com>
2019-02-15 19:42:08 +0300

    TikTok added

--- src/components/Interface.vue
+++ src/components/Interface.vue
@@ -13,48 +13,84 @@
       </v-alert>
     </v-flex>
 
-    <div class="padding"></div>
-        
-      <v-flex xs12 md6 offset-xs3>
-        <v-select attach :items="items" v-model="site" @click="inputClicked"></v-select>
-      </v-flex>
+  <div class="padding"></div>
 
-      <v-flex xs12 md6 offset-xs3>
-        <v-text-field value label="URL" v-model="url" clearable @click="inputClicked"></v-text-field>
-      </v-flex>
-
-      <v-flex xs12 md6 offset-xs3>
-        <v-layout row wrap>
-          <v-text-field value label="Word" v-model="word" clearable @click="inputClicked"></v-text-field>
-
-          <v-btn @click="inputChanged" outline dark large color="primary">
+  <v-flex xs12 md6 offset-xs3>
+    <v-card class="box">
+      <v-layout row wrap>
+        <v-text-field v-on:keyup.enter="getYoutube" value label="YouTube URL" v-model="youtubeUrl" clearable ></v-text-field>
+        <v-btn @click="getYoutube" outline dark large color="red">
             <v-icon dark>search</v-icon>
-          </v-btn>
+        </v-btn>
+      </v-layout>
+      <v-flex xs6 md6 offset-xs3>
+        <v-progress-linear color="red" :active="isYT" :indeterminate="true"></v-progress-linear>
+      </v-flex> 
+      <h3 class="text-md-center">
+        <v-chip label outline color="red">
+          <h3>{{youtubeNum}}</h3>
+        </v-chip> encounters
+      </h3>   
+    </v-card>
+  </v-flex>
+
+
+  <v-flex xs12 md6 offset-xs3>
+    <v-card class="box">
+    <v-layout row wrap>
+      <v-text-field v-on:keyup.enter="getInstagram" value label="Instagram URL" v-model="instagramUrl" clearable></v-text-field>
+      <v-btn @click="getInstagram" outline dark large color="orange">
+            <v-icon dark>search</v-icon>
+        </v-btn>
+    </v-layout>
+    <v-flex xs6 md6 offset-xs3>
+      <v-progress-linear color="orange" :active="isIN" :indeterminate="true"></v-progress-linear>
+    </v-flex>  
+    <h3 class="text-md-center">
+      <v-chip label outline color="orange">
+        <h3>{{instagramNum}}</h3>
+      </v-chip> encounters
+    </h3>
+    </v-card>
+  </v-flex>
+
+  <v-flex xs12 md6 offset-xs3>
+    <v-card class="box">
+      <v-layout row wrap>
+        <v-text-field v-on:keyup.enter="getTiktok" value label="TikTok URL" v-model="tiktokUrl" clearable></v-text-field>
+        <v-btn @click="getTiktok" outline dark large color="primary">
+                <v-icon dark>search</v-icon>
+            </v-btn>
+      </v-layout>
+      <v-flex xs6 md6 offset-xs3>
+        <v-progress-linear :active="isTT" :indeterminate="true"></v-progress-linear>
+      </v-flex>
+      <h3 class="text-md-center">
+        <v-chip label outline color="primary">
+          <h3>{{tiktokNum}}</h3>
+        </v-chip> encounters
+      </h3>
+    </v-card>
+  </v-flex>
+
+
+  <v-dialog v-model="dialog" max-width="350">
+      <v-card>
+        <v-card-title class="headline">TikTok captcha</v-card-title>
+        <v-layout wrap row>
+          <img src="http://127.0.0.1:5000/captcha/cp.png" style="margin-left: 50px;" id = "image" height="100" width="250">
+          <v-text-field v-on:keyup.enter="captchaEnter" style="margin-left: 50px; margin-right: 50px;" value label="TikTok captcha" v-model="captcha" clearable></v-text-field>
         </v-layout>
-      </v-flex>
 
-      <!--v-flex xs12 md6 offset-xs3>
-          <v-select
-            :items="items"
-            attach
-            chips
-            label="Socail networks"
-            multiple
-            @input="inputChanged"
-          ></v-select>
-      </v-flex-->
-      <v-flex xs6 md6 offset-xs3>
-        <v-progress-linear :active="isLoad" :indeterminate="true"></v-progress-linear>
-      </v-flex>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" @click="captchaEnter" @keyup.enter="captchaEnter"> Enter</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
-      <div style="height:50px;"></div>
-      <v-flex xs6 md6 offset-xs3>
-        <h3 class="text-md-center">The word encountered
-          <v-chip label outline color="red">
-            <h3>{{encounters}}</h3>
-          </v-chip>times
-        </h3>
-      </v-flex>
+  
+
     </v-layout>
   </v-container>
 </template>
@@ -62,62 +98,135 @@
 <script>
 export default {
   data: () => ({
-    items: ["YouTube", "Instagram", "Facebook", "TikTok"],
-    site: "YouTube",
-    url: "",
-    word: "",
-    isLoad: false,
-    isErr: false,
-    encounters: 0
-  }),
+    youtubeUrl: "",
+    instagramUrl: "",
+    tiktokUrl: "",
 
+    youtubeNum: 0,
+    instagramNum: 0,
+    tiktokNum: 0,
+
+    isYT: false,
+    isIN: false,
+    isTT: false,
+    isErr: false,
+
+    dialog: false,
+    captcha: "",
+  }),
+computed: {
+  example: {
+    cache: false,
+    get: function () {
+      return Date.now() + this.msg
+    }
+  }
+},
   methods: {
-    async getNumWords(site, url, word) {
+    async getYoutube() {
+      this.isErr = false;
+      this.isYT = true;
       try {
-        let answer = await this.makeReq({ site, url, word });
+        let answer = await this.makeReq({ site:"youtube", url: this.youtubeUrl });
         /* eslint-disable no-console */
         console.log(`getNumWords got result`, answer);
-        this.encounters = answer;
+        this.youtubeNum = answer;
       } catch (error) {
         /* eslint-disable no-console */
         console.log(`getNumWords got error`, error);
         this.isErr = true;
-        this.encounters = 0;
+        this.youtubeNum = 0;
       } finally {
-        this.isLoad = false;
+        this.isYT = false;
       }
     },
 
-    inputClicked: function() {
+    async getInstagram() {
+      
       this.isErr = false;
-    },
+      this.isIN = true;
+      try {
+        let answer = await this.makeReq({ site:"instagram", url: this.instagramUrl });
+        /* eslint-disable no-console */
+        console.log(`getNumWords got result`, answer);
 
-    inputChanged: function() {
-      this.isLoad = true;
-      switch (this.site) {
-        case "YouTube":
-          this.getNumWords("youtube", this.url, this.word);
-          break;
-        case "Instagram":
-          this.getNumWords("instagram", this.url, this.word);
-          break;
-        case "Facebook":
-          this.getNumWords("facebook", this.url, this.word);
-          break;
-        case "TikTok":
-          this.getNumWords("tiktok", this.url, this.word);
-          break;
+        this.instagramNum = answer;
+      } catch (error) {
+        /* eslint-disable no-console */
+        console.log(`getNumWords got error`, error);
+        this.isErr = true;
+        this.instagramNum = 0;
+      } finally {
+        this.isIN = false;
       }
     },
-    makeReq({ site, url, word }) {
+    
+    captchaEnter: function(){
+      this.getTiktok(this.captcha);
+      this.dialog = false;
+    },
+
+    async getTiktok(_captcha) {
+      // Kostyl, call from search button
+      if(typeof(_captcha)== "object"){
+        _captcha=""
+        this.captcha = ""
+      }
+
+      this.isErr = false;
+      this.isTT = true;
+      
+      try {
+        let answer = await this.makeReqTikt({ site:"tiktok", 
+        url: this.tiktokUrl, captcha: _captcha});
+        /* eslint-disable no-console */
+        if (answer == "image"){
+          this.dialog = true;
+          //console.log("Answer: "+ answer)
+          let img = document.getElementById("image");
+          img.src = "http://127.0.0.1:5000/captcha/cp.png?dummy="+ Math.random(0, 1000)
+        } else {
+            this.tiktokNum = answer;
+        }
+     
+      } catch (error) {
+        /* eslint-disable no-console */
+        console.log(`getNumWords got error`, error);
+        this.isErr = true;
+        this.tiktokNum = 0;
+      } finally {
+        this.isTT = false;
+        this.captcha = ""
+      }
+    },
+
+    makeReq({ site, url }) {
       return new Promise((resolve, reject) => {
         this.$http
-          .get(`http://10.1.1.192:5000/${site}`, {
-            params: { url: url, keyword: word }
+          .get(`http://127.0.0.1:5000/${site}`, {
+            params: { url: url }
           })
           .then(response => {
             /* eslint-disable no-console */
             console.log(`response.bodyText`, response.bodyText);
+            return resolve(response.bodyText);
+          })
+          .catch(err => {
+            console.log(`makeReq got err`, err);
+            return reject(err);
+          });
+      });
+    },
+
+    makeReqTikt({ site, url, captcha }) {
+      return new Promise((resolve, reject) => {
+        this.$http
+          .get(`http://127.0.0.1:5000/${site}`, {
+            params: { url: url, captcha: captcha}
+          })
+          .then(response => {
+            /* eslint-disable no-console */
+            console.log(`response`, response);
             return resolve(response.bodyText);
           })
           .catch(err => {
@@ -132,6 +241,12 @@ export default {
 
 <style>
 .padding {
-  height: 150px;
+  height: 100px;
+}
+.box{
+  padding-top:30px;
+  padding-left:10px;
+  padding-right:10px;
+  margin-bottom: 50px;
 }
 </style>
